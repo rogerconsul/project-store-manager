4bd2c38304deaacf2989c6063e0f7cabdcb76832
Roger Consul <rogerconsul@yahoo.com.br>
2022-07-06 15:59:55 -0300

    Iniciando testagem da aplicacao

---
+++ tests/unit/controllers/productsController.test.js
@@ -0,0 +1,47 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+
+chai.use(chaiAsPromised);
+
+const controller = require('../../../controllers/productsController');
+const service = require('../../../services/productsService');
+
+describe('productsController', () => {
+  beforeEach(sinon.restore);
+
+  describe('Testa o método REMOVE', () => {
+    it('Deve disparar erro caso o service.remove dispare', () => {
+      sinon.stub(service, 'remove').rejects();
+      return chai.expect(controller.remove({}, {})).to.eventually.be.rejected
+    })
+
+    it('Deve resolver', async () => {
+      sinon.stub(service, 'remove').resolves(1);
+      const res = {
+        status: sinon.stub().callsFake(() => res),
+        json: sinon.stub().returns(),
+        end: sinon.stub().returns(),
+      }
+      await controller.remove({params: {id: 1}}, res)
+      chai.expect(res.status.getCall(0).args[0]).to.equal(204)
+    })
+
+    it('Deve vir invalido na funcao exclui', async () => {
+      sinon.stub(service, 'remove').resolves(false);
+      const res = {
+        status: sinon.stub().callsFake(() => res),
+        json: sinon.stub().returns({ message: 'Product not found' }),
+      }
+      await controller.remove({params: {id: 1}}, res)
+      chai.expect(res.status.getCall(0).args[0]).to.equal(404)
+      chai.expect(res.json.getCall(0).args[0]).to.deep.equal({ message: 'Product not found' })
+    })
+  })
+
+  describe('Teste o método UPDATE', () => {
+    it('Deve disparar erro caso o service.update dê erro', async () => {
+      sinon.stub(service, 'update').rejects();
+    })
+  })
+})
--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -1,6 +1,48 @@
-const { expect } = require('chai');
-const { beforeEach, afterEach } = require('mocha');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
-const connection = require('../../../models/connection');
-const salesController = require('../../../controllers/salesController');
 
+chai.use(chaiAsPromised);
+
+const controller = require('../../../controllers/salesController');
+const service = require('../../../services/salesService');
+const model = require('../../../models/sales');
+
+
+describe('salesController', () => {
+  beforeEach(sinon.restore);
+
+  describe('Testa o método getAll', () => {
+    it('Deve disparar erro caso o service.getAll dispare', () => {
+      sinon.stub(service, 'getAll').rejects();
+      return chai.expect(controller.getAll({}, {})).to.eventually.be.rejected
+    })
+
+    // it('Deve resolver', async () => {
+    //   sinon.stub(service, 'remove').resolves();
+    //   const res = {
+    //     status: 204,
+    //   }
+    //   chai.expect(res.status).to.equal(204)
+    // })
+  })
+
+  describe('Teste o método getById', () => {
+    it('Deve disparar erro caso o service.getById dê erro', async () => {
+      sinon.stub(service, 'getById').rejects();
+      return chai.expect(controller.getById({}, {})).to.eventually.be.rejected
+    })
+  })
+
+  describe('Teste o método create', () => {
+    it('Deve disparar erro caso o idFinder dê erro', async () => {
+      sinon.stub(model, 'idFinder').rejects();
+      return chai.expect(controller.create({}, {})).to.eventually.be.rejected
+    })
+
+    it('Deve disparar erro caso o service.manageDate dê erro', async () => {
+      sinon.stub(service, 'manageDate').rejects();
+      return chai.expect(controller.create({}, {})).to.eventually.be.rejected
+    })
+  })
+})
